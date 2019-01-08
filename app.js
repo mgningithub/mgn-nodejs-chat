7000a4d836db4faacf3d27108295b08a5af70e00
mgn <mobius1.missile.missile@gmail.com>
2019-01-08 14:20:06 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var http = require('http');
+var socketio = require('socket.io');
+var fs = require('fs');
+
+// xxxx番ポートでHTTPサーバを立てる
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(fs.readFileSync('./index.html', 'utf-8'));
+}).listen(process.env.PORT || 8000);
+
+// サーバにソケットを紐づけ
+var io = socketio.listen(server);
+
+// 接続確立後の通信処理を定義
+io.sockets.on('connection', function (socket) {
+
+    // クライアントからサーバへ、メッセージ送信ハンドラ（自分を含む全員宛て）
+    socket.on('c2s_message', function (data) {
+        // サーバからクライアントへ、メッセージ送り返し
+        io.sockets.emit('s2c_message', { value: data.value });
+    });
+
+    // クライアントからサーバへ、メッセージ送信ハンドラ（自分以外の全員宛て）
+    socket.on('c2s_broadcast', function (data) {
+        // サーバからクライアントへ、メッセージ送り返し
+        socket.broadcast.emit('s2c_message', { value: data.value });
+    });
+
+});
